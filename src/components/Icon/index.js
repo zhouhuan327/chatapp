62ab9cfcf76921b954b871e7cd177449e1ece4d7
sean <zsean21598@163.com>
2020-06-19 22:58:16 +0800

    增加icon的color和opacity属性

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,17 +1,17 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import  StyledIcon from './style.js';
-function Icon({ children, ...rest }) {
+import StyledIcon from './style.js';
+function Icon({ icon: IconComponent, width = 24, height = 24, color, opacity, ...rest }) {
     return (
-        <StyledIcon {...rest}>
-           {children}
+        <StyledIcon color={color} opacity={opacity} {...rest}>
+            {IconComponent && <IconComponent width={width} height={height} />}
         </StyledIcon>
     );
 }
 
 Icon.propTypes = {
-   children:PropTypes.any
+    children: PropTypes.any
 };
 
 export default Icon;
--- src/components/Icon/style.js
+++ src/components/Icon/style.js
@@ -1,6 +1,15 @@
 
 import styled from 'styled-components';
 
-const StyledIcon = styled.div``;
+const StyledIcon = styled.div`
+    display:inline-flex;
+    align-items:center;
+    justify-content:center;
+
+    svg,svg *{
+        ${({ color }) => (color ? `fill:${color}` : "")};
+        ${({ opacity }) => (opacity ? `opacity:${opacity}` : "")};
+    }
+`;
 
 export default StyledIcon;
